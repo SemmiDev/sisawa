b10fcb517179dac256cdd7b97a6188331e4f87bf
Sammi Aldhi Yanto <55814880+SemmiDev@users.noreply.github.com>
2024-07-16 21:20:44 +0700

    add

--- resources/views/layouts/bab/tembang_dolanan.blade.php
+++ resources/views/layouts/bab/tembang_dolanan.blade.php
@@ -24,7 +24,6 @@
     @include("layouts.bab.$alias.tab_dolanan")
 @endsection
 
+
 @section('icon-tab-1', Vite::asset("resources/images/materi/tabListItemPesan.png"))
-@section('icon-tab-0', Vite::asset("public/images/bab/tembang_dolanan.png"))
-
-
+@section('icon-tab-0', Vite::asset("resources/images/bab/tembang_dolanan.png"))
