822245b0f35a7e6fb017c3e8eeb6268f35beeb50
Sammi Aldhi Yanto <55814880+SemmiDev@users.noreply.github.com>
2024-07-18 15:47:26 +0700

    dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,10 +17,12 @@
 
     <!-- Pustaka Ikon Lucide -->
     <script src="https://cdn.jsdelivr.net/npm/lucide@latest/dist/umd/lucide.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/1.3.1/js/toastr.js"></script>
         <!-- Include SweetAlert -->
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/1.3.1/css/toastr.css" rel="stylesheet"/>
         <link href="toastr.css" rel="stylesheet"/>
         <script src="toastr.js"></script>
     @vite('resources/css/app.css')
